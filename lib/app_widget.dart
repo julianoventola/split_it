00a507c99afb127b0127f1e76f2b5c51d5fa3dda
Juliano Ventola <ventolajuliano@gmail.com>
2021-05-25 01:47:01 -0300

    feat: splash page

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:split_it/modules/splash/splash_page.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Split.it',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: SplashPage(),
     );
   }
 }
