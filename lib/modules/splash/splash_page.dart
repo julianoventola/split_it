39a8c63452103b8a458afbe6a2a91d0215323769
Juliano Ventola <ventolajuliano@gmail.com>
2021-05-25 02:23:13 -0300

    feat: squares to complement splash page

--- lib/modules/splash/splash_page.dart
+++ lib/modules/splash/splash_page.dart
@@ -12,13 +12,88 @@ class SplashPage extends StatelessWidget {
         decoration: BoxDecoration(
           gradient: AppTheme.gradients.background,
         ),
-        child: Center(
-          child: Image.asset(
-            'assets/images/Logo.png',
-            width: MediaQuery.of(context).size.width * 0.3,
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              SquaresSplashPage(
+                rowAligment: MainAxisAlignment.start,
+                firstChildSquare: Image.asset(
+                  'assets/images/retangulo_esquerda.png',
+                  width: 198,
+                  height: 98,
+                ),
+                secondChildSquare: Image.asset(
+                  'assets/images/retangulo_esquerda.png',
+                  width: 114,
+                  height: 58,
+                ),
+              ),
+              Image.asset(
+                'assets/images/Logo.png',
+                width: 128,
+                height: 112,
+              ),
+              SquaresSplashPage(
+                rowAligment: MainAxisAlignment.end,
+                firstChildSquare: Image.asset(
+                  'assets/images/retangulo_direita.png',
+                  width: 114,
+                  height: 58,
+                ),
+                secondChildSquare: Image.asset(
+                  'assets/images/retangulo_direita.png',
+                  width: 198,
+                  height: 98,
+                ),
+              ),
+            ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class SquaresSplashPage extends StatelessWidget {
+  final Image firstChildSquare;
+  final Image secondChildSquare;
+  final MainAxisAlignment rowAligment;
+
+  const SquaresSplashPage({
+    Key? key,
+    required this.firstChildSquare,
+    required this.secondChildSquare,
+    required this.rowAligment,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: rowAligment,
+          children: [
+            Opacity(
+              opacity: 0.2,
+              child: firstChildSquare,
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 24,
+        ),
+        Row(
+          mainAxisAlignment: rowAligment,
+          children: [
+            Opacity(
+              opacity: 0.2,
+              child: secondChildSquare,
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
