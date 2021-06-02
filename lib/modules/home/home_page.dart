439cac5d04f9d9def221a567c7cbded866d80c4f
Juliano Ventola <ventolajuliano@gmail.com>
2021-06-02 02:00:56 -0300

    feat: display event model in a list

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -2,10 +2,66 @@ import 'package:flutter/material.dart';
 import 'package:split_it/modules/home/widgets/event_tile.dart';
 import 'package:split_it/modules/home/widgets/home_app_bar.dart';
 import 'package:split_it/modules/login/models/user_model.dart';
+import 'package:split_it/shared/models/event_model.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final events = [
+    EventModel(
+      title: 'Churrasco',
+      created: DateTime.now(),
+      value: 50.5,
+      friends: 2,
+    ),
+    EventModel(
+      title: 'Aniversário',
+      created: DateTime.now().subtract(Duration(days: 20)),
+      value: -145.5,
+      friends: 13,
+    ),
+    EventModel(
+      title: 'Churrasco',
+      created: DateTime.now(),
+      value: 50.5,
+      friends: 2,
+    ),
+    EventModel(
+      title: 'Aniversário',
+      created: DateTime.now().subtract(Duration(days: 20)),
+      value: -145.5,
+      friends: 13,
+    ),
+    EventModel(
+      title: 'Churrasco',
+      created: DateTime.now(),
+      value: 50.5,
+      friends: 2,
+    ),
+    EventModel(
+      title: 'Aniversário',
+      created: DateTime.now().subtract(Duration(days: 20)),
+      value: -145.5,
+      friends: 13,
+    ),
+    EventModel(
+      title: 'Churrasco',
+      created: DateTime.now(),
+      value: 50.5,
+      friends: 2,
+    ),
+    EventModel(
+      title: 'Aniversário',
+      created: DateTime.now().subtract(Duration(days: 20)),
+      value: -145.5,
+      friends: 13,
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     final UserModel user =
@@ -17,16 +73,13 @@ class HomePage extends StatelessWidget {
         addButtonOnTap: () {},
       ),
       body: Container(
-        padding: EdgeInsets.fromLTRB(32, 40, 32, 16),
+        padding: EdgeInsets.fromLTRB(32, 40, 32, 0),
         child: ListView(
-          children: [
-            EventTile(
-              title: 'Churrasco',
-              subTitle: '01 março',
-              value: 50.35,
-              friends: '2 amigos',
-            ),
-          ],
+          children: events
+              .map((event) => EventTile(
+                    model: event,
+                  ))
+              .toList(),
         ),
       ),
     );
--- lib/modules/home/widgets/event_tile.dart
+++ lib/modules/home/widgets/event_tile.dart
@@ -1,28 +1,28 @@
 import 'package:flutter/material.dart';
 
 import 'package:split_it/modules/home/widgets/icon_dollar.dart';
+import 'package:split_it/shared/models/event_model.dart';
 import 'package:split_it/theme/app_theme.dart';
 
+import 'package:intl/intl.dart';
+
 class EventTile extends StatelessWidget {
-  final String title;
-  final String subTitle;
-  final double value;
-  final String friends;
+  final EventModel model;
 
   const EventTile({
     Key? key,
-    required this.title,
-    required this.subTitle,
-    required this.value,
-    required this.friends,
+    required this.model,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    var valueToDisplay = (value > 0 ? value : value.abs())
+    final DateFormat formatter = DateFormat('dd MMM');
+
+    var valueToDisplay = (model.value > 0 ? model.value : model.value.abs())
         .toStringAsFixed(2)
         .replaceAll(".", ",");
-    var dollarType = value > 0 ? IconDollarType.send : IconDollarType.receive;
+    var dollarType =
+        model.value > 0 ? IconDollarType.send : IconDollarType.receive;
     return Row(
       children: [
         IconDollar(type: dollarType),
@@ -35,11 +35,11 @@ class EventTile extends StatelessWidget {
                 ListTile(
                   contentPadding: EdgeInsets.all(0),
                   title: Text(
-                    title,
+                    model.title,
                     style: AppTheme.textStyle.eventTileTitle,
                   ),
                   subtitle: Text(
-                    subTitle,
+                    formatter.format(model.created),
                     style: AppTheme.textStyle.eventTileSubTitle,
                   ),
                   trailing: Column(
@@ -53,7 +53,7 @@ class EventTile extends StatelessWidget {
                         height: 5,
                       ),
                       Text(
-                        friends,
+                        '${model.friends} pessoa${model.friends == 1 ? '' : 's'}',
                         style: AppTheme.textStyle.eventTileFriend,
                       ),
                     ],
